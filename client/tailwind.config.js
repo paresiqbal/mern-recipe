9e5bb423b3e1c725676ee80476f91bef0be705b5
paresiqbal <thecrowdel@gmail.com>
2023-06-08 23:04:42 +0700

    add custom config tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "green-base": "#DAF7A6",
+        "yellow-base": "#FFC300",
+        "orange-base": "#FF5733",
+        "red-base": "#C70039",
+        "dark-red": "#900C3F",
+        "dark-purple": "#581845 ",
+      },
+      fontFamily: {
+        Poppins: ["Poppins, sans-serif"],
+      },
+      container: {
+        padding: "2rem",
+        center: true,
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+      },
+    },
   },
   plugins: [],
 };
