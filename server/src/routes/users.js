1153d5233099ac306b166b62f50b70b52b0350c8
paresiqbal <thecrowdel@gmail.com>
2023-06-08 21:18:28 +0700

    Login function complete

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -25,6 +25,22 @@ router.post("/register", async (req, res) => {
   res.json({ message: "Registered Successfully" });
 });
 
-router.post("/login");
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  // Find username in modal
+  const user = await UserModel.findOne({ username });
+
+  if (!user) return res.json({ message: "User does't exist !" });
+
+  // Compare input user password with real password
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.json({ message: "Username or Password is incorrect !" });
+  }
+
+  const token = jwt.sign({ id: user._id }, "secret");
+  res.json({ token, userID: user.id });
+});
 
 export { router as userRouter };
