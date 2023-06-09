f97031e7dc7bcddf6d8fa3c11158cf4217fc60f1
paresiqbal <thecrowdel@gmail.com>
2023-06-09 20:12:24 +0700

    Add custom color

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        padding: "2rem",
+        center: true,
+      },
+      colors: {
+        "base-green": "#DAF7A6",
+        "base-yelow": "#FFC300 ",
+        "base-orange": "#FF5733",
+        "base-red": "#C70039 ",
+        "base-purple": "#900C3F ",
+        "dark-purple": "#581845",
+      },
+    },
   },
   plugins: [],
 };
