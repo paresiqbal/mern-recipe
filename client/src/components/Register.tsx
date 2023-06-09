713db157878f8487820461dd6aeccbaadf5bd164
paresiqbal <thecrowdel@gmail.com>
2023-06-09 20:09:12 +0700

    Trying next time baby

--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -1,7 +1,14 @@
 export default function Register() {
   return (
     <form>
-      <label htmlFor=""></label>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input type="text" name="username" id="username" />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+      </div>
     </form>
   );
 }
