868a8f441bd426dbc350efb002c72ca480e4e45b
paresiqbal <thecrowdel@gmail.com>
2023-06-09 20:28:15 +0700

    tabs complete

--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -1,12 +1,34 @@
+// React
+import { useState } from "react";
+
 // Components
 import Login from "../components/Login";
 import Register from "../components/Register";
 
 export default function Auth() {
+  const [activeTab, setActiveTab] = useState("login");
+
+  const handleTabChange = (tab: any) => {
+    setActiveTab(tab);
+  };
+
   return (
     <div>
-      <Login />
-      <Register />
+      <div>
+        <button
+          onClick={() => handleTabChange("login")}
+          className={activeTab === "login" ? "active" : ""}
+        >
+          Login
+        </button>
+        <button
+          onClick={() => handleTabChange("register")}
+          className={activeTab === "register" ? "active" : ""}
+        >
+          Register
+        </button>
+      </div>
+      {activeTab === "login" ? <Login /> : <Register />}
     </div>
   );
 }
