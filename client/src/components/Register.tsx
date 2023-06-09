9480abbf28f2068af63217c39be3ea1a2cee113e
paresiqbal <thecrowdel@gmail.com>
2023-06-09 20:19:49 +0700

    Add state

--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -1,13 +1,31 @@
+import { useState } from "react";
+
 export default function Register() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <form>
+      <h1>Register</h1>
       <div>
         <label htmlFor="username">Username</label>
-        <input type="text" name="username" id="username" />
+        <input
+          type="text"
+          name="username"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
       </div>
       <div>
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
     </form>
   );
