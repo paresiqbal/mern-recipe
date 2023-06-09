9d08296fbae418e5b064640509a512d56211c48d
paresiqbal <thecrowdel@gmail.com>
2023-06-09 20:23:02 +0700

    login complete

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,3 +1,34 @@
+// React
+import { useState } from "react";
+
 export default function Login() {
-  return <div>Login</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <form>
+      <h1>Login</h1>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button type="submit">Register</button>
+    </form>
+  );
 }
