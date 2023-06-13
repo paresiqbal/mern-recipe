cc03844488c118361b00ea0a991487c7d28af2c9
paresiqbal <thecrowdel@gmail.com>
2023-06-13 23:07:21 +0700

    add form name

---
+++ client/src/pages/CreateRecipe.tsx
@@ -0,0 +1,11 @@
+export default function CreateRecipe() {
+  return (
+    <div>
+      <h2>Create Recipe</h2>
+      <form action="">
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" />
+      </form>
+    </div>
+  );
+}
