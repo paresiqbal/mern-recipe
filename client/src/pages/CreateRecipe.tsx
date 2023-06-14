ff0331f68af2a4b73094c8d845c282e422a1f79f
paresiqbal <thecrowdel@gmail.com>
2023-06-14 09:44:14 +0700

    add style in form input recipe

--- client/src/pages/CreateRecipe.tsx
+++ client/src/pages/CreateRecipe.tsx
@@ -17,42 +17,75 @@ export default function CreateRecipe() {
   };
 
   return (
-    <div>
-      <h2>Create Recipe</h2>
-      <form action="">
-        <div>
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" onChange={handleInput} />
+    <div className="p-10 flex flex-col items-center">
+      <h2 className="text-xl font-semibold">Create Recipe</h2>
+      <form action="" className="flex flex-col gap-2">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Name
+          </label>
+          <input
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            id="name"
+            type="text"
+            placeholder="Name"
+          />
         </div>
-        <div>
-          <label htmlFor="ingridients">Ingridients</label>
+        <div className="flex flex-col">
+          <label
+            htmlFor="ingridients"
+            className="block text-gray-700 text-sm font-bold mb-2"
+            placeholder="Ingridients"
+          >
+            Ingridients
+          </label>
         </div>
-        <div>
-          <label htmlFor="instruction">Instruction</label>
+        <div className="flex flex-col">
+          <label
+            htmlFor="instruction"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Instruction
+          </label>
           <textarea
             name="instruction"
             id="instruction"
-            cols={20}
-            rows={30}
+            rows={5}
+            placeholder="Insctruction"
             onChange={handleInput}
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
           ></textarea>
         </div>
-        <div>
-          <label htmlFor="imageUrl">ImageUrl</label>
+        <div className="flex flex-col">
+          <label
+            htmlFor="imageUrl"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            ImageUrl
+          </label>
           <input
             type="text"
             name="imageUrl"
             id="imageUrl"
+            placeholder="Image"
             onChange={handleInput}
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
           />
         </div>
-        <div>
-          <label htmlFor="cookingTime">Cooking Time</label>
+        <div className="flex flex-col">
+          <label
+            htmlFor="cookingTime"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Time
+          </label>
           <input
-            type="number"
-            id="cookingTime"
+            type="text"
             name="cookingTime"
+            id="cookingTime"
             onChange={handleInput}
+            placeholder="30 Minute"
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
           />
         </div>
       </form>
