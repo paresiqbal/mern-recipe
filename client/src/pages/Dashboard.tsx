550512ae1461edae2ce88208fff12539213b10d0
paresiqbal <thecrowdel@gmail.com>
2023-06-14 11:29:59 +0700

    Show recipe

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,14 +1,39 @@
+// React
+import axios from "axios";
+import { useEffect, useState } from "react";
 export default function Dashboard() {
+  const [recipes, setRecipes] = useState([]);
+  const [savedRecipes, setSavedRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/recipes");
+        setRecipes(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchRecipes();
+  }, []);
   return (
     <div>
-      <h1 className="text-center text-xl font-bold text-orange-500">
-        Dashboard
-      </h1>
-      <p className="text-sm text-center">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, ducimus
-        nisi. Veniam quo alias doloribus eaque reprehenderit vero dolores
-        tempore?
-      </p>
+      <h1>Recipes</h1>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe._id}>
+            <div>
+              <h2>{recipe.name}</h2>
+            </div>
+            <div className="instructions">
+              <p>{recipe.instructions}</p>
+            </div>
+            <img src={recipe.imageUrl} alt={recipe.name} />
+            <p>Cooking Time: {recipe.cookingTime} minutes</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
